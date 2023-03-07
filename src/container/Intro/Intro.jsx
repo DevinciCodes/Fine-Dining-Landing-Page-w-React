8b9d303bd89d221815f69ada5571dd44965019c9
Devin Latham <devinlatham20@gmail.com>
2023-03-07 08:09:43 -0600

    create layout and first few components

---
+++ src/container/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BsFillPlayFill, BsPauseFill} from 'react-icons/bs'
+import{ meal } from '../../constants'
+
+import './Intro.css';
+
+const Intro = () => (
+  <div className='app__video'>
+    <video src={meal}/>
+  </div>
+);
+
+export default Intro;
